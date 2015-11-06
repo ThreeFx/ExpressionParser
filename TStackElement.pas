79be7c974dd455fdb8432f6ad8ecfbf0141ec960
Markus Himmel <markus@himmel-villmar.de>
2015-11-06 16:02:28 +0100

    Update TStackElement.pas

--- TStackElement.pas
+++ TStackElement.pas
@@ -1,1 +1,25 @@
 UNIT StackElement;
+
+INTERFACE
+	TYPE
+		Operator = FUNCTION(operands : ARRAY OF TStackValue) : Integer;
+	
+		TStackElement = CLASS
+		END;
+	
+		TStackValue = CLASS(TStackValue)
+		END;
+		
+		TStackOperator = CLASS(TStackOperator)
+		PRIVATE
+			CLASS VAR Plus, Minus, Times, DividedBy, UnaryMinus : TStackOperator;
+			
+		PUBLIC
+			CLASS FUNCTION OpPlus() : TStackOperator;
+			CLASS FUNCTION OpMinus() : TStackOperator;
+			CLASS FUNCTION OpTimes() : TStackOperator;
+			CLASS FUNCTION OpDividedBy() : TStackOperator;
+			CLASS FUNCTION OpUnaryMinus() : TStackOperator;
+			
+			
+		END;
