864e6c9ea2d954ab1904a5bf0c4c7b818b9fabef
Ben Fiedler <ben@sjfiedler.de>
2015-11-06 14:58:48 +0100

    Create Parser.pas

---
+++ Parser.pas
@@ -0,0 +1,16 @@
+UNIT Parser;
+
+USES SysUtils; // more to come
+
+INTERFACE
+
+	FUNCTION ParseExpr(expr : String) : TNode;
+	
+IMPLEMENTATION
+	
+	FUNCTION ParseExpr(expr : String) : TNode;
+	BEGIN
+	END;
+
+BEGIN
+END.
