e94715836341309164f2e02c716cd888aba9c3df
Ben Fiedler <ben@sjfiedler.de>
2015-11-10 14:23:35 +0100

    Should work now

--- Parser.pas
+++ Parser.pas
@@ -1,6 +1,6 @@
 UNIT Parser;
 
-USES Utils, SysUtils; // more to come
+USES TNode, Utils, SysUtils; // more to come
 
 INTERFACE
 
@@ -11,7 +11,7 @@ IMPLEMENTATION
 	CONST Operators = ['+', '-', '*'];
 	
 	// 0 = no operator, 1 = unary op, 2 = binary op
-	FUNCTION GetOpType(expr : String; index, lower, upper : Integer) : Integer;
+	FUNCTION GetOpType(expr : String; index, lower : Integer) : Integer;
 	BEGIN
 		result := 0;
 		IF Contains<Char>(Operators, expr[index]) THEN
@@ -28,16 +28,16 @@ IMPLEMENTATION
 		END;
 	END;
 	
-	FUNCTION IsOp(expr : String; index, lower, upper : Integer) : Boolean;
+	FUNCTION IsOp(expr : String; index, lower : Integer) : Boolean;
 	BEGIN
 		result := GetOpType(expr, index, lower) > 0;
 	END;
 	
-	FUNCTION IsUnOp(expr : String; index, lower, upper : Integer) : Boolean;
+	FUNCTION IsUnOp(expr : String; index, lower : Integer) : Boolean;
 	BEGIN
 		result := GetOpType(expr, index, lower) = 1;
 	
-	FUNCTION IsBinOp(expr : String; index, lower, upper : Integer)
+	FUNCTION IsBinOp(expr : String; index, lower : Integer)
 	BEGIN
 		result := GetOpType(expr, index, lower) = 2;
 	END;
@@ -48,24 +48,30 @@ IMPLEMENTATION
 			'+': result := 1;
 			'-': result := 1;
 			'*': result := 2;
+			'/': result := 2;
 		END;
 	END;
 	
 	FUNCTION HasNextBinaryOperator(expr : String; index, lower, upper : Integer) : Boolean;
 	BEGIN
-		IF index = upper + 1 THEN
+		IF index > upper THEN
 			result := false
-		ELSE IF IsBinOp(expr, uppe, lower) THEN
+		ELSE IF IsBinOp(expr, index, lower) THEN
 			result := true
 		ELSE
 			result := HasNextBinaryOperator(expr, index + 1, lower, upper);
 	
-	FUNCTION GetIndexOfNextBinaryOperator(expr : String; index, lower, upper : Integer) : Integer;
+	FUNCTION GetIndexOfNextBinaryOperator(expr : String; index, lower, upper : Integer) : Integer; OVERLOAD;
 	BEGIN
-		IF IsBinOp(expr, lower) THEN
+		IF IsBinOp(expr, index, lower) THEN
 			result := index
 		ELSE
-			result := GetIndexOfNextBinaryOperator(expr, lower + 1, upper);
+			result := GetIndexOfNextBinaryOperator(expr, index + 1, lower, upper);
+	END;
+	
+	FUNCTION GetIndexOfNextBinaryOperator(expr : String; lower, upper : Integer) : Integer; OVERLOAD;
+	BEGIN
+		result := GetIndexOfNextBinaryOperator(expr, lower, lower, upper);
 	END;
 	
 	FUNCTION GetIndexOfNextOperator(expr : String; lower, upper : Integer) : Integer;
@@ -80,7 +86,7 @@ IMPLEMENTATION
 		BEGIN
 			IF HasNextBinaryOperator(expr, lower, upper) THEN
 			BEGIN
-				precedence := GetOperatorPrecedence([GetIndexOfNextBinaryOperator(expr, index, lower, upper)]);
+				precedence := GetOperatorPrecedence(expr[GetIndexOfNextBinaryOperator(expr, lower, upper)]);
 				IF precedence < minimum THEN
 				BEGIN
 					minimum := precedence;
@@ -88,6 +94,16 @@ IMPLEMENTATION
 				END;
 			END;
 			Inc(index);
+		END;
+	END;
+	
+	FUNCTION CreateBinOperator(op : Char; leftChild, rightChild : TNode) : TNode;
+	BEGIN
+		CASE op OF
+			'+': result := TAdd.Create(leftChild, rightChild);
+			'-': result := TSub.Create(leftChild, rightChild);
+			'*': result := TMul.Create(leftChild, rightChild);
+			'-': result := TDiv.Create(leftChild, rightChild);
 		END;
 	END;
 		
@@ -98,9 +114,20 @@ IMPLEMENTATION
 		IF HasNextBinaryOperator(expr, lower, upper) THEN
 		BEGIN
 			nextBinOpIndex := GetIndexOfNextOperator(expr, lower, lower, upper);
-			result := TBinaryOperator.Create(
-			                                 ParseExp(expr, lower, lower, index - 1),
-			                                 ParseExp(expr, index + 1, upper);
+			result := CreateBinOperator(
+				expr[nextBinOpIndex],
+				ParseExp(expr, lower, lower, index - 1),
+				ParseExp(expr, nextBinOpIndex + 1, upper)
+			);
+		END
+		ELSE IF expr[lower] = '-'
+		BEGIN
+			result := TMin.Create(ParseExpr(expr, lower + 1, upper));
+		END
+		ELSE
+		BEGIN
+			result := TValue.Create(StrToInt(SubStr(lower, upper - lower + 1)));
+		END;
 	END;
 	
 	FUNCTION ParseExpr(expr : String) : TNode; OVERLOAD;
