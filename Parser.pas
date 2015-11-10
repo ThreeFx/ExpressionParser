672bf168c623cb8d2b6e0b11ba2b3de789e0b19b
Ben Fiedler <ben@sjfiedler.de>
2015-11-10 15:13:57 +0100

    Works probably

--- Parser.pas
+++ Parser.pas
@@ -1,27 +1,44 @@
 UNIT Parser;
 
-USES Node, Utils, SysUtils; // more to come
-
 INTERFACE
 
+        USES Node, Utils, StrUtils, SysUtils; // more to come
+
 	FUNCTION ParseExpr(expr : String) : TNode; OVERLOAD;
+
+        TYPE CharSet = Set of Char;
 	
 IMPLEMENTATION
 	
-	CONST Operators = ['+', '-', '*'];
-	
+	CONST Operators = ['+', '-', '*', '/'];
+
+        FUNCTION Contains(arr : CharSet; element : Char) : Boolean;
+        VAR
+                val : Char;
+        BEGIN
+		result := false;
+		FOR val IN arr DO
+		BEGIN
+			IF val = element THEN
+			BEGIN
+				result := true;
+				exit;
+			END;
+		END;
+	END;
+
 	// 0 = no operator, 1 = unary op, 2 = binary op
 	FUNCTION GetOpType(expr : String; index, lower : Integer) : Integer;
 	BEGIN
 		result := 0;
-		IF Contains<Char>(Operators, expr[index]) THEN
+		IF Contains(Operators, expr[index]) THEN
 		BEGIN
 			// Unary operator per default
 			result := 1;
 			
 			// But if the Character preceding it is not an operator, it is a binary operator
 			// Ex. 5 * - 7
-			IF (NOT (index = lower)) AND (NOT IsOp(expr, index - 1)) THEN
+			IF (NOT (index = lower)) AND (NOT Contains(Operators, expr[index - 1])) THEN
 			BEGIN
 				result := 2;
 			END;
@@ -36,8 +53,9 @@ IMPLEMENTATION
 	FUNCTION IsUnOp(expr : String; index, lower : Integer) : Boolean;
 	BEGIN
 		result := GetOpType(expr, index, lower) = 1;
-	
-	FUNCTION IsBinOp(expr : String; index, lower : Integer)
+        END;
+
+	FUNCTION IsBinOp(expr : String; index, lower : Integer) : Boolean;
 	BEGIN
 		result := GetOpType(expr, index, lower) = 2;
 	END;
@@ -52,7 +70,7 @@ IMPLEMENTATION
 		END;
 	END;
 	
-	FUNCTION HasNextBinaryOperator(expr : String; index, lower, upper : Integer) : Boolean;
+	FUNCTION HasNextBinaryOperator(expr : String; index, lower, upper : Integer) : Boolean; OVERLOAD;
 	BEGIN
 		IF index > upper THEN
 			result := false
@@ -60,19 +78,30 @@ IMPLEMENTATION
 			result := true
 		ELSE
 			result := HasNextBinaryOperator(expr, index + 1, lower, upper);
-	
-	FUNCTION GetIndexOfNextBinaryOperator(expr : String; index, lower, upper : Integer) : Integer; OVERLOAD;
+
+        END;
+
+        FUNCTION HasNextBinaryOperator(expr : String; lower, upper : Integer) : Boolean; OVERLOAD;
+        BEGIN
+                result := HasNextBinaryOperator(expr, lower, lower, upper);
+        END;
+
+	FUNCTION GetIndexOfNextBinaryOperator(expr : String; index, lower, upper : Integer) : Integer;
 	BEGIN
 		IF IsBinOp(expr, index, lower) THEN
-			result := index
+                BEGIN
+			result := index;
+                END
 		ELSE
+                BEGIN
 			result := GetIndexOfNextBinaryOperator(expr, index + 1, lower, upper);
+                END;
 	END;
 	
-	FUNCTION GetIndexOfNextBinaryOperator(expr : String; lower, upper : Integer) : Integer; OVERLOAD;
-	BEGIN
-		result := GetIndexOfNextBinaryOperator(expr, lower, lower, upper);
-	END;
+	//FUNCTION GetIndexOfNextBinaryOperator(expr : String; lower, upper : Integer) : Integer; OVERLOAD;
+	//BEGIN
+	//	result := GetIndexOfNextBinaryOperator(expr, lower, lower, upper);
+	//END;
 	
 	FUNCTION GetIndexOfNextOperator(expr : String; lower, upper : Integer) : Integer;
 	VAR
@@ -84,10 +113,10 @@ IMPLEMENTATION
 		minimum := 100000;
 		WHILE index <= upper DO
 		BEGIN
-			IF HasNextBinaryOperator(expr, lower, upper) THEN
+			IF HasNextBinaryOperator(expr, index, upper) THEN
 			BEGIN
-				precedence := GetOperatorPrecedence(expr[GetIndexOfNextBinaryOperator(expr, lower, upper)]);
-				IF precedence < minimum THEN
+				precedence := GetOperatorPrecedence(expr[GetIndexOfNextBinaryOperator(expr, index, lower, upper)]);
+				IF precedence <= minimum THEN
 				BEGIN
 					minimum := precedence;
 					result := index;
@@ -103,7 +132,7 @@ IMPLEMENTATION
 			'+': result := TAdd.Create(leftChild, rightChild);
 			'-': result := TSub.Create(leftChild, rightChild);
 			'*': result := TMul.Create(leftChild, rightChild);
-			'-': result := TDiv.Create(leftChild, rightChild);
+			'/': result := TDiv.Create(leftChild, rightChild);
 		END;
 	END;
 		
@@ -113,20 +142,23 @@ IMPLEMENTATION
 	BEGIN
 		IF HasNextBinaryOperator(expr, lower, upper) THEN
 		BEGIN
-			nextBinOpIndex := GetIndexOfNextOperator(expr, lower, lower, upper);
+			nextBinOpIndex := GetIndexOfNextOperator(expr, lower, upper) + 1;
+                        WriteLn(expr[nextBinOpIndex]);
+                        //WriteLn(nextBinOpindex);
 			result := CreateBinOperator(
 				expr[nextBinOpIndex],
-				ParseExp(expr, lower, lower, index - 1),
-				ParseExp(expr, nextBinOpIndex + 1, upper)
+				ParseExpr(expr, lower, nextBinOpIndex - 1),
+				ParseExpr(expr, nextBinOpIndex + 1, upper)
 			);
 		END
-		ELSE IF expr[lower] = '-'
+		ELSE IF expr[lower] = '-' THEN
 		BEGIN
 			result := TMin.Create(ParseExpr(expr, lower + 1, upper));
 		END
 		ELSE
-		BEGIN
-			result := TValue.Create(StrToInt(SubStr(lower, upper - lower + 1)));
+                BEGIN
+                        //WriteLn(Copy(expr, lower, upper - lower + 1));
+			result := TValue.Create(StrToInt(Copy(expr, lower, upper - lower + 1)));
 		END;
 	END;
 	
