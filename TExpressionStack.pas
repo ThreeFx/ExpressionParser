8959dbc5190d00d4585c39aae9e92eaebbd4d248
Markus Himmel <markus@himmel-villmar.de>
2015-11-10 15:13:27 +0100

    Update TExpressionStack.pas

--- TExpressionStack.pas
+++ TExpressionStack.pas
@@ -1,30 +1,34 @@
-UNIT ExpressionStack;
+ UNIT ExpressionStack;
 
-USES StackElement;
+
 
 INTERFACE
+        USES StackElement;
 
 	TYPE
 		TExpressionStack = CLASS
 			PRIVATE
 				underlying : ARRAY OF TStackElement;
 				FUNCTION pop() : TStackElement;
+                FUNCTION operatorOnTop() : Boolean;
+                FUNCTION Print() : String;
 			PUBLIC
 				CONSTRUCTOR Create();
 				DESTRUCTOR Dispose();
-	
+
 				PROCEDURE Push(elem : TStackElement);
 				FUNCTION Evaluate() : Integer;
+
 		END;
 
-IMPLEMENTATION
+IMPLEMENTATION USES SysUtils;
 
 	CONSTRUCTOR TExpressionStack.Create();
 	BEGIN
 		SetLength(underlying, 0);
 	END;
-	
-	DESTRUCTOR TExpressionStack.Dispose()
+
+	DESTRUCTOR TExpressionStack.Dispose();
 	VAR
 		elem : TStackElement;
 	BEGIN
@@ -34,36 +38,62 @@ IMPLEMENTATION
 		END;
 		SetLength(underlying, 0);
 	END;
-	
+
 	FUNCTION TExpressionStack.pop() : TStackElement;
 	BEGIN
 		result := underlying[High(underlying)];
 		SetLength(underlying, Length(underlying) - 1);
 	END;
-	
+
 	PROCEDURE TExpressionStack.Push(elem : TStackElement);
 	BEGIN
-		SetLenngth(underlying, Length(underlying) + 1);
+		SetLength(underlying, Length(underlying) + 1);
 		underlying[High(underlying)] := elem;
 	END;
-	
-	PROCEDURE TExpressionStack.Evaluate() : Integer;
+
+        FUNCTION TExpressionStack.operatorOnTop() : Boolean;
+        BEGIN
+             result := underlying[High(underlying)] IS TStackOperator;
+        END;
+
+        FUNCTION TExpressionStack.Print() : String;
+        VAR
+                elem : TStackElement;
+        BEGIN
+             result := '';
+             FOR elem in underlying DO
+             BEGIN
+                     if elem IS TStackValue THEN BEGIN
+                       result := result + IntToStr((elem AS TStackValue).GetValue());
+                     end
+                     else begin
+                       result := result + 'Op';
+                     end;
+                     result := result + ' ';
+             end;
+        END;
+
+	FUNCTION TExpressionStack.Evaluate() : Integer;
 	VAR
 		pending : ARRAY OF TStackValue;
-		current : TStackElement
+		current : TStackElement;
 	BEGIN
 		SetLength(pending, 0);
-		WHILE Length(underlying) > 1 DO
+		WHILE (Length(underlying) > 1) OR (operatorOnTop) DO
 		BEGIN
+			WriteLn(Print);
 			current := pop();
 			IF current IS TStackOperator THEN
 			BEGIN
-				Push(TStackValue.Create(current.Evaluate(pending)));
+				Push(TStackValue.Create((current AS TStackOperator).Evaluate(pending)));
+				SetLength(pending, 0);
 			END
 			ELSE BEGIN
 				SetLength(pending, Length(pending) + 1);
-				pending[High(pending)] := current;
+				pending[High(pending)] := (current AS TStackValue);
 			END;
 		END;
 		result := (pop() AS TStackValue).GetValue();
 	END;
+BEGIN
+END.
