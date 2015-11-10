6881ecae3d13775e793017bf9d32a70f0494b7cd
Markus Himmel <markus@himmel-villmar.de>
2015-11-10 14:18:10 +0100

    Update TStackElement.pas

--- TStackElement.pas
+++ TStackElement.pas
@@ -7,19 +7,52 @@ INTERFACE
 		TStackElement = CLASS
 		END;
 	
-		TStackValue = CLASS(TStackValue)
+		TStackValue = CLASS(TStackElement)
+			PRIVATE
+				theValue : Integer;
+				
+			PUBLIC
+				CONSTRUCTOR Create(val : Integer);
+				FUNCTION GetValue() : Integer;
 		END;
 		
-		TStackOperator = CLASS(TStackOperator)
-		PRIVATE
-			CLASS VAR Plus, Minus, Times, DividedBy, UnaryMinus : TStackOperator;
+		TStackOperator = CLASS(TStackElement)
+			PRIVATE
+				CLASS VAR Plus, Minus, Times, DividedBy, UnaryMinus : TStackOperator;
 			
-		PUBLIC
-			CLASS FUNCTION OpPlus() : TStackOperator;
-			CLASS FUNCTION OpMinus() : TStackOperator;
-			CLASS FUNCTION OpTimes() : TStackOperator;
-			CLASS FUNCTION OpDividedBy() : TStackOperator;
-			CLASS FUNCTION OpUnaryMinus() : TStackOperator;
+			PUBLIC
+				CLASS FUNCTION OpPlus() : TStackOperator;
+				CLASS FUNCTION OpMinus() : TStackOperator;
+				CLASS FUNCTION OpTimes() : TStackOperator;
+				CLASS FUNCTION OpDividedBy() : TStackOperator;
+				CLASS FUNCTION OpUnaryMinus() : TStackOperator;
 			
 			
 		END;
+
+IMPLEMENTATION
+
+	FUNCTION Add(operands : ARRAY OF TStackValue) : Integer;
+	BEGIN
+		result := operands[0].GetValue() + operands[1].GetValue();
+	END;
+	
+	FUNCTION Sub(operands : ARRAY OF TStackValue) : Integer;
+	BEGIN
+		result := operands[0].GetValue() + operands[1].GetValue();
+	END;
+	
+	FUNCTION Mul(operands : ARRAY OF TStackValue) : Integer;
+	BEGIN
+		result := operands[0].GetValue() + operands[1].GetValue();
+	END;
+	
+	FUNCTION Div(operands : ARRAY OF TStackValue) : Integer;
+	BEGIN
+		result := operands[0].GetValue() + operands[1].GetValue();
+	END;
+	
+	FUNCTION Neg(operands : ARRAY OF TStackValue) : Integer;
+	BEGIN
+		result := operands[0].GetValue() + operands[1].GetValue();
+	END;
