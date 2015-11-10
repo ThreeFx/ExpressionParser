262cf6cc2f4727eea54b44f5b28d63b774c3445b
Markus Himmel <markus@himmel-villmar.de>
2015-11-10 14:28:56 +0100

    Update TStackElement.pas

--- TStackElement.pas
+++ TStackElement.pas
@@ -19,7 +19,8 @@ INTERFACE
 		TStackOperator = CLASS(TStackElement)
 			PRIVATE
 				CLASS VAR Plus, Minus, Times, DividedBy, UnaryMinus : TStackOperator;
-			
+				CONSTRUCTOR Create(op : Operator);
+				
 			PUBLIC
 				CLASS FUNCTION OpPlus() : TStackOperator;
 				CLASS FUNCTION OpMinus() : TStackOperator;
@@ -32,6 +33,16 @@ INTERFACE
 
 IMPLEMENTATION
 
+	CONSTRUCTOR TStackValue.Create(val : Integer);
+	BEGIN
+		theValue := val;
+	END;
+	
+	FUNCTION TStackValue.GetValue() : Integer;
+	BEGIN
+		result := theValue;
+	END;
+
 	FUNCTION Add(operands : ARRAY OF TStackValue) : Integer;
 	BEGIN
 		result := operands[0].GetValue() + operands[1].GetValue();
@@ -39,20 +50,53 @@ IMPLEMENTATION
 	
 	FUNCTION Sub(operands : ARRAY OF TStackValue) : Integer;
 	BEGIN
-		result := operands[0].GetValue() + operands[1].GetValue();
+		result := operands[0].GetValue() - operands[1].GetValue();
 	END;
 	
 	FUNCTION Mul(operands : ARRAY OF TStackValue) : Integer;
 	BEGIN
-		result := operands[0].GetValue() + operands[1].GetValue();
+		result := operands[0].GetValue() * operands[1].GetValue();
 	END;
 	
-	FUNCTION Div(operands : ARRAY OF TStackValue) : Integer;
+	FUNCTION Divi(operands : ARRAY OF TStackValue) : Integer;
 	BEGIN
-		result := operands[0].GetValue() + operands[1].GetValue();
+		result := operands[0].GetValue() div operands[1].GetValue();
 	END;
 	
 	FUNCTION Neg(operands : ARRAY OF TStackValue) : Integer;
 	BEGIN
-		result := operands[0].GetValue() + operands[1].GetValue();
+		result := -operands[0].GetValue();
 	END;
+	
+	CLASS FUNCTION TStackOperator.OpUnaryMinus() : TStackOperator;
+	BEGIN
+		result := Minus;
+	END;
+	
+	CLASS FUNCTION TStackOperator.OpPlus() : TStackOperator;
+	BEGIN
+		result := Plus;
+	END;
+	
+	CLASS FUNCTION TStackOperator.OpDividedBy() : TStackOperator;
+	BEGIN
+		result := DividedBy;
+	END;
+	
+	CLASS FUNCTION TStackOperator.OpTimes() : TStackOperator;
+	BEGIN
+		result := Times;
+	END;
+	
+	CLASS FUNCTION TStackOperator.OpUnaryMinus() : TStackOperator;
+	BEGIN
+		result := UnaryMinus;
+	END;
+
+	BEGIN
+		TStackOperator.Plus = TStackOperator.Create(@Add);
+		TStackOperator.Minus = TStackOperator.Create(@Sub);
+		TStackOperator.Times = TStackOperator.Create(@Mul);
+		TStackOperator.DividedBy = TStackOperator.Create(@Divi);
+		TStackOperator.UnaryMinus = TStackOperator.Create(@Neg);
+	END.
